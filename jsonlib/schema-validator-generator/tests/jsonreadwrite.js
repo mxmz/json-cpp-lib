d67a299b68f8c1b0eb184de8ed7f2d534cec6c87
Maxim <maxim@mxmz.net>
2014-03-18 16:54:23 +0100

    removed autogen, added .gitignore

--- jsonlib/schema-validator-generator/tests/jsonreadwrite.js
+++ jsonlib/schema-validator-generator/tests/jsonreadwrite.js
@@ -5,7 +5,7 @@ console.log(token_request_schemas);
 
 token_request_schemas.assert_token_request_json1(
                 {
-                    "grant_type": "remotes",
+                    "grant_type": "remote",
                     "type": "pippo",
                     "token": "asdasdasd",
                     "scope": "asd asd asda "
